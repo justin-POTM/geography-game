076c5dbb0fb38aad71dbff900b29354e142f1de5
justin-POTM <justin0819.yl@gmail.com>
2026-07-16 04:18:19 +0800

    Add service worker for asset caching
    Implement service worker for caching assets.

---
+++ sw.js
@@ -0,0 +1,16 @@
+const CACHE_NAME = 'geo-game-v1';
+const ASSETS = [
+  './index.html',
+  './icon-192.png',
+  './icon-512.png',
+  'https://unpkg.com/leaflet/dist/leaflet.css',
+  'https://unpkg.com/leaflet/dist/leaflet.js'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
+});
